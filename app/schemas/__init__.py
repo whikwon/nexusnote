f1889ce4dafd4fe3cf3b82dabf7f8758ee37e513
whikwon <whikwon@gmail.com>
2025-02-15 15:43:44 +0900

    minor fix

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -2,6 +2,7 @@ from .annotation import AnnotationBase, AnnotationCreate, AnnotationUpdate
 from .block import BlockBase, BlockCreate, BlockUpdate
 from .concept import ConceptBase, ConceptCreate, ConceptUpdate
 from .document import DocumentBase, DocumentCreate, DocumentUpdate
+from .link import LinkCreate
 from .msg import Msg
 from .rag import RAGRequest, RAGResponse
 
@@ -18,6 +19,7 @@ __all__ = [
     "DocumentBase",
     "DocumentCreate",
     "DocumentUpdate",
+    "LinkCreate",
     "Msg",
     "RAGRequest",
     "RAGResponse",
